c3b404cbc64712b810bf7ddff62e3e248468046e
= <oliver@kaarnalehto.fi>
2017-03-23 18:56:44 +0200

    Plans, Controller, Routes

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -5,10 +5,31 @@
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
    	  View::make('home.html');
+      //echo "Tämä on etusivu!";
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make("helloworld.html");
+    }
+
+    public static function login() {
+      View::make("plans/login.html");
+    }
+
+    public static function feed() {
+      View::make("plans/feed.html");
+    }
+
+    public static function notifications() {
+      View::make("plans/notifications.html");
+    }
+
+    public static function profile() {
+      View::make("plans/profile.html");
+    }
+
+    public static function settings() {
+      View::make("plans/settings.html");
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,24 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+
+  $routes->get('/feed', function() {
+    HelloWorldController::feed();
+  });
+
+  $routes->get('/notifications', function() {
+    HelloWorldController::notifications();
+  });
+
+  $routes->get('/profile', function() {
+    HelloWorldController::profile();
+  });
+
+  $routes->get('/settings', function() {
+    HelloWorldController::settings();
+  });
